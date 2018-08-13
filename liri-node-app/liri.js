8458ef5a41b3e2856cb39bca9c1084f29a9fee8b
wjcooper91 <wjcooper91@gmail.com>
2018-08-12 20:16:32 -0400

    added friend finder

--- liri-node-app/liri.js
+++ liri-node-app/liri.js
@@ -54,22 +54,19 @@ function getTweets() {
 
 }
 
-// function getSpotify() {
-//     var spotify = new Spotify({
-//         id: '1ec1606d0ccc46c99bf8138ba507074c',
-//         secret: '5c36fb68a30f47fab907e79f2e2eeb2b',
-//       });
+function getSpotify() {
+    
        
-//       spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//         if (err) {
-//           return console.log('Error occurred: ' + err);
-//         }
+      spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
        
-//       console.log(data); 
-//       });
+      console.log(data); 
+      });
 
-// }
-// getSpotify();
+}
+getSpotify();
 
 
 var fs = require('fs');
