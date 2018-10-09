7f9140f159350c38ef358666a80ca18711745fd2
wjcooper91 <wjcooper91@gmail.com>
2018-10-08 21:53:56 -0400

    added files

---
+++ click-app/src/components/Score/Score.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Score.css";
+
+//stateless component
+const Score = props => (
+  <div className="gameScore">
+    <h3 className="score">Your Score >{props.total}</h3>
+    <h3 className="status">{props.status}</h3>
+  </div>
+);
+
+export default Score;
